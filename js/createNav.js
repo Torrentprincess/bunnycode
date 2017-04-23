80e45bcd0a1ed40f020abbf1aece17652abccb19
Ellie <j.chen.ellie@gmail.com>
2017-04-22 23:45:37 -0400

    NAVIGATION IS COMPLETE. Only need to adjust colors. (Updated shared.css & updated createNav.js)

---
+++ js/createNav.js
@@ -0,0 +1,34 @@
+document.getElementById("nav").innerHTML = ' <div id="logo"> <span>bunnyCode</span> </div> <div id="nav-text-container"> <ul> <li class="bhover"> <a>HTML</a> <ul class="dropdown"> <li>HTML Fundamentals</li> <li>HTML Content</li> </ul> </li><!-- --><li class="bhover"> <a>CSS</a> <ul class="dropdown"> <li>The CSS Box Model</li> <li>Styling with CSS</li> <li>Organizing HTML & CSS</li> </ul> </li> </ul> </div>'
+    
+
+/*
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+User-readable 
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+            <div id="logo">
+                <span>bunnyCode</span>
+            </div>
+            <div id="nav-text-container">
+                <ul>
+                    <li class="bhover">
+                        <a>HTML</a>
+                        <ul class="dropdown">
+                            <li>HTML Fundamentals</li>
+                            <li>HTML Content</li>
+                        </ul>
+                    </li><!--
+                    --><li class="bhover">
+                        <a>CSS</a>
+                        <ul class="dropdown">
+                            <li>The CSS Box Model</li>
+                            <li>Styling with CSS</li>
+                            <li>Organizing HTML & CSS</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+*/            
