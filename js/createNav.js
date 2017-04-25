4cb89a1272513989be385415e291ec15241f0a27
Ellie <j.chen.ellie@gmail.com>
2017-04-25 13:29:33 +0000

    Updated createNav.js

--- js/createNav.js
+++ js/createNav.js
@@ -1,4 +1,4 @@
-document.getElementById("nav").innerHTML = '<div id="logo"> <a href="/content/html/test/Introduction.html"><span>bunnyCode</span></a> </div> <div id="nav-text-container"> <ul> <li class="bhover"> <a>HTML</a> <ul class="dropdown"> <a href="/content/html/1/html_fundamentals.html"><li>HTML Fundamentals</li></a> <a href="/content/html/2/html_content.html"><li>HTML Content</li></a> <a href="/content/html/3/css_fundamentals.html"><li>CSS Fundamentals</li></a> <a href="/content/html/4/styling_css.html"><li>Styling CSS</li></a> <a href="/content/html/5/organizing_html.html"><li>Organizing HTML</li></a> </ul> </li><!-- --><li class="bhover"> <a>CSS</a> <ul class="dropdown"> <a href="/content/css/6/box.html"><li>The CSS Box Model</li> <a href="/content/css/7/position.html"><li>Images</li> <a href="/content/css/8/images.html"><li>Tables</li> </ul> </li> </ul> </div>'
+document.getElementById("nav").innerHTML = '<div id="logo"> <a href="/"> <span>bunnyCode</span> </a> </div> <div id="nav-text-container"> <ul> <li class="bhover"> <a>HTML</a> <ul class="dropdown"> <a href="/content/html/1/html_fundamentals.html"><li>HTML Fundamentals</li></a> <a href="/content/html/2/html_content.html"><li>HTML Content</li></a> <a href="/content/html/3/css_fundamentals.html"><li>CSS Fundamentals</li></a> <a href="/content/html/4/styling_css.html"><li>Styling CSS</li></a> <a href="/content/html/5/organizing_html.html"><li>Organizing HTML</li></a> </ul> </li><!-- --><li class="bhover"> <a>CSS</a> <ul class="dropdown"> <a href="/content/css/6/box.html"><li>The CSS Box Model</li> <a href="/content/css/7/position.html"><li>Images</li> <a href="/content/css/8/images.html"><li>Tables</li> </ul> </li> </ul> </div>'
     
 
 /*
@@ -10,11 +10,12 @@ User-readable
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
             
-            <a href="/content/html/test/Introduction.html">
-                <div id="logo">
+            
+            <div id="logo">
+                <a href="/">
                     <span>bunnyCode</span>
-                </div>
-            </a>
+                </a>
+            </div>
             <div id="nav-text-container">
                 <ul>
                     <li class="bhover">
